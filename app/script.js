51779d3c8442b4991bd99fb84d3a034964025293
ramajeyam-mediwave <ramajeyam@mindwaveventures.com>
2023-09-29 11:17:12 +0530

    modified

--- app/script.js
+++ app/script.js
@@ -1,30 +1,43 @@
 $(document).ready(function () {
-  
+
+  const loadingIndicator = $("#loadingIndicator");
+
+    function showLoading() {
+    loadingIndicator.show();
+  }
+
+  function hideLoading() {
+    loadingIndicator.hide();
+  }
+                            
   function populateDropdown(dogBreeds) {
     const dropdown = $("#breedDropdown");
     dropdown.empty();
     dropdown.append('<option value="">Select a breed</option>');
 
-    dogBreeds.forEach(function (breed) {
+    for (let i = 0; i < dogBreeds.length; i++) {
+      const breed = dogBreeds[i];
       dropdown.append(`<option value="${breed}">${breed}</option>`);
-    });
+    }
   }
 
-  $("#breedDropdown").change(function () {
-    const selectedBreed = $(this).val();
+  $("#button").click(function () {
+    //change is a event that exsicute when there is a change in ui
+    const selectedBreed = $("#breedDropdown").val();//this,val is used to take cuurent value in drop down box
 
     if (selectedBreed) {
       getRandomImageOfDog(selectedBreed);
     }
   });
-function getAllDogsFromApi() {
+
+  function getAllDogsFromApi() { // this function is used to makes an AJAX GET request to the dog API to retrieve a list of all dog breeds.
     const url = "https://dog.ceo/api/breeds/list/all";
     $.ajax(url, {
       method: "GET",
       success: function (resp) {
         console.log("API request success");
         const dogsList = Object.keys(resp.message);
-        console.log(dogsList); 
+        console.log(dogsList);
         populateDropdown(dogsList);
       },
       error: function () {
@@ -32,19 +45,20 @@ function getAllDogsFromApi() {
       },
       complete: function () {
         console.log("API request completed");
-      },
-    });
+        },
+       });
+    
   }
-
+    //The code snippet defines a function called getRandomImageOfDog that takes a dog breed as input and makes an AJAX GET request to the dog API to retrieve a random image of a dog of that breed.
   function getRandomImageOfDog(dogBreed) {
     const url = `https://dog.ceo/api/breed/${dogBreed}/images/random`;
+    showLoading(); 
     $.ajax(url, {
       method: "GET",
       success: function (resp) {
         console.log("API request success");
         console.log(resp);
-
-        
+        hideLoading();
         displayImage(resp.message);
       },
       error: function () {
@@ -52,22 +66,17 @@ function getAllDogsFromApi() {
       },
     });
   }
-
+  //every time the image url change the argument is also change and start calling the function
   function displayImage(imageUrl) {
     const imageContainer = $("#imageContainer");
 
-   imageContainer.empty();
+    imageContainer.empty();
 
-    
     const imgElement = $("<img>");
     imgElement.attr("src", imageUrl);
 
- 
     imageContainer.append(imgElement);
   }
 
-  
   getAllDogsFromApi();
 });
-
-
